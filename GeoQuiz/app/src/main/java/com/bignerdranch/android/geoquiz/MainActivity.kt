90b0af277a1fcc646778aca1ffef18b9229415c4
moacirjun <moacirjunior.eletro@gmail.com>
2021-12-24 00:21:49 -0300

    Chapter 01 :tada:

---
+++ GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.bignerdranch.android.geoquiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        trueButton = findViewById(R.id.true_button)
+        falseButton = findViewById(R.id.false_button)
+
+        trueButton.setOnClickListener {
+            Toast.makeText(this, R.string.correct, Toast.LENGTH_SHORT).show()
+        }
+        falseButton.setOnClickListener {
+            Toast.makeText(this, R.string.incorrect, Toast.LENGTH_SHORT).show()
+        }
+    }
+}
