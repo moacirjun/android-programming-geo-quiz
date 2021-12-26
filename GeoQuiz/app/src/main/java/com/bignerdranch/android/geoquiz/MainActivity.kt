f9eba60efebcdff9dc96cce9aeb72632e5c6d3a7
moacirjun <moacirjunior.eletro@gmail.com>
2021-12-26 00:22:02 -0300

    Chapter 03

--- GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -2,9 +2,12 @@ package com.bignerdranch.android.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,6 +29,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreated() called.")
         setContentView(R.layout.activity_main)
 
         trueButton = findViewById(R.id.true_button)
@@ -45,6 +49,31 @@ class MainActivity : AppCompatActivity() {
         }
 
         updateQuestion()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called.")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResumed() called.")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPaused() called.")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called.")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called.")
     }
 
     private fun updateQuestion() {
